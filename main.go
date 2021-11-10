3ef0ba0618c41fd5ef5074a896072df1ecdde380
Tamal Saha <tamal@appscode.com>
2021-11-10 00:22:32 -0800

    added-all
    Signed-off-by: Tamal Saha <tamal@appscode.com>

--- main.go
+++ main.go
@@ -1,5 +1,21 @@
 package main
-
+import (
+	"os"
+	"text/template"
+)
 func main() {
-
+	type Inner struct {
+		A string
+	}
+	type Outer struct {
+		Inner
+	}
+	o := Outer{
+		Inner: Inner{A : "123"},
+	}
+	tpl := template.Must(template.New("").Option("missingkey=error").Parse(`{{.A}} {{ .B }}`))
+	err := tpl.Execute(os.Stdout, &o)
+	if err != nil {
+		panic(err)
+	}
 }
